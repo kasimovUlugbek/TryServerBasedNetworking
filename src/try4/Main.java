1cc00541912dd346cd4a792eee6e6a715c14df60
Ulugbek Kasimov <ukasimov957@student.fuhsd.org>
2024-04-29 18:04:21 -0700

    esperate try package

---
+++ src/try4/Main.java
@@ -0,0 +1,25 @@
+package try4;
+
+import networking.frontend.NetworkManagementPanel;
+import processing.core.PApplet;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		drawing.windowResizable(true);
+
+		NetworkManagementPanel nmp = new NetworkManagementPanel("ProcessingAction", 6, drawing);
+
+		
+		
+	}
+	
+	
+	
+	
+
+}
+
---
+++ src/try4/PlayerData.java
@@ -0,0 +1,25 @@
+package try4;
+
+import java.io.Serializable;
+
+
+/**
+ * 
+ * In general, it is smart to make custom classes representing the data that you'd like to send
+ * over the network. 
+ * 
+ * @author john_shelby
+ *
+ */
+public class PlayerData implements Serializable {
+	
+	private static final long serialVersionUID = -6050080578707829643L;
+	
+	public String uniqueID;
+	public String username;
+	public double x, y;
+	public int r, g, b;
+	public int shapeType;
+	
+
+}
