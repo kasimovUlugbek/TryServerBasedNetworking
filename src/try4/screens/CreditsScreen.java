3a4020bf42424af4759ad343d2e677d3f9287468
Evan Wescott <evo.wescott@gmail.com>
2024-04-30 21:23:13 -0700

    Update CreditsScreen.java

--- src/try4/screens/CreditsScreen.java
+++ src/try4/screens/CreditsScreen.java
@@ -11,5 +11,21 @@ public class CreditsScreen extends Screen {
 		super(800, 600);
 		this.surface = surface;
 	}
+	
+	public void draw() {
+		surface.background(255);
+		
+		surface.fill(0);
+		String str = "Bloodbath Brigade™, made by Evan Wescott.";
+		float w = surface.textWidth(str);
+		surface.text(str, (surface.width - w) / 2, surface.height / 2);
+	}
+	
+	public void keyPressed() {
+		if (surface.key == PApplet.ESC) {
+			surface.switchScreen(ScreenSwitcher.MENU_SCREEN);
+			surface.key = 0;
+		}
+	}
 
 }
